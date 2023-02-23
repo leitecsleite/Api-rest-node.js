76286e6e90e8dfb828943cc5521771a6ab97db82
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-02-23 11:19:53 -0300

    Feat(master):Added route plugging.

---
+++ src/routes/transactions.ts
@@ -0,0 +1,26 @@
+import { randomUUID } from 'node:crypto'
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { Knex } from '../../src/database/database'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await Knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
+  })
+}
