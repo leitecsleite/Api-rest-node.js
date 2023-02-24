b0eede7d20dcfb6bacac5591ef84bb6b8670c8eb
Lucas Sousa <70826073+leitecsleite@users.noreply.github.com>
2023-02-23 22:22:35 -0300

    Feat(master): Added method get to fetch transactions.

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,24 @@ import { z } from 'zod'
 import { Knex } from '../../src/database/database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await Knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transactions = await Knex('transactions').where('id', id).first()
+
+    return { transactions }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
